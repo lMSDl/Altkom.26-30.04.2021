ceb9ed14a1b14498206f0553276c11ed00faa0e6
Paweł Łęczycki <pawel@msd.info.pl>
2021-04-30 13:22:55 +0200

    OCP

---
+++ DobrePraktyki/SOLID/II_OpenClose.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DobrePraktyki.SOLID.O
+{
+    abstract class Shape
+    {
+        public abstract double Area { get; }
+    }
+
+    class Square : Shape
+    {
+        public int A { get; set; }
+
+        public override double Area => A * A;
+    }
+
+    class Rectangle : Shape
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public override double Area => A * B;
+    }
+    class Circle : Shape
+    {
+        public int R { get; set; }
+        public override double Area => R * R * Math.PI;
+    }
+
+    class ShapeCalculator
+    {
+        double Area(Shape shape)
+        {
+            return shape.Area;
+        }
+    }
+}
