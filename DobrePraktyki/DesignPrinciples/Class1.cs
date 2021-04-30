d0b435b1a42451646dffd6ea006fedb07e7eb1e0
Paweł Łęczycki <pawel@msd.info.pl>
2021-04-30 12:08:25 +0200

    DesignPrinciples

---
+++ DobrePraktyki/DesignPrinciples/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DobrePraktyki.DesignPrinciples
+{
+    public class Customer
+    {
+        public Customer(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+        public string CustomerName { get; set; }
+        public float Income { get; set; }
+        public float Outcome { get; set; }
+
+        private float allowedDebit;
+
+        public float GetAllowedDebit()
+        {
+            return allowedDebit;
+        }
+
+        public void SetAllowedDebit(float value)
+        {
+            allowedDebit = value;
+        }
+
+        public bool GetDebit()
+        {
+            return Income - Outcome < 0;
+        }
+    }
+
+    public class CustomerService
+    {
+        private ICollection<Customer> Customers { get; } = new List<Customer> { new Customer(1), new Customer(2), new Customer(3), new Customer(4), new Customer(5) };
+
+        public bool DeleteCustomer(Customer customer)
+        {
+            return Customers.Remove(customer);
+        }
+
+        public Customer FindByDebit(float debit)
+        {
+            return Customers.SingleOrDefault(x => x.GetAllowedDebit() == debit);
+        }
+
+        public bool Charge(int customerId, float amount)
+        {
+            var customer = Customers.SingleOrDefault(x => x.Id == customerId);
+            if (customer == null)
+                return false;
+
+            if (customer.Income - customer.Outcome + customer.GetAllowedDebit() < amount)
+                return false;
+
+            customer.Outcome += amount;
+            return true;
+        }
+
+        public void Fund(int customerId, float amount)
+        {
+            var customer = Customers.Where(x => x.Id == customerId).SingleOrDefault();
+            if (customer == null)
+                return;
+            customer.Income += amount;
+        }
+
+        public float? GetBalance(int customerId)
+        {
+            var customer = Customers.Where(x => x.Id == customerId).SingleOrDefault();
+            return customer?.Income - customer?.Outcome;
+        }
+    }
+}
