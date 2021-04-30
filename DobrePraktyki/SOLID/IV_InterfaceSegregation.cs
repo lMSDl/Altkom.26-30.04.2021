e8c660b7d01960b0d8b77f4d10421daa5df1f98d
Paweł Łęczycki <pawel@msd.info.pl>
2021-04-30 13:43:40 +0200

    ISP

---
+++ DobrePraktyki/SOLID/IV_InterfaceSegregation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DobrePraktyki.SOLID.I
+{
+    internal interface IExcelFormatter
+    {
+        void ToExcel();
+    }
+    internal interface IPdfFormatter
+    { 
+        void ToPdf();
+    }
+
+    class Report : IExcelFormatter, IPdfFormatter
+    {
+        public void ToExcel()
+        {
+            Console.WriteLine("Excel generated");
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+
+    class Poem : IPdfFormatter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+}
